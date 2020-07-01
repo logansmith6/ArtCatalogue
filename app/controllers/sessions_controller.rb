6688a49b016677bc42d4fd632be40ae0ab01bef4
logansmith6 <logsmith06@gmail.com>
2020-06-30 19:10:12 -0700

    added session/new.html.erb

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController 
+
+    def new
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            flash[:success] = 'You are now signed in.'
+            redirect_to ''
+        else
+            flash[:warning] = 'Invalid Username or Password'
+            redirect_to '/login'
+    end
+end 
