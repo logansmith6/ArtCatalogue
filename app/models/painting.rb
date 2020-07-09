77bc8abc8ac17341faff9c8cdad46eb7e0a28bfd
logansmith6 <logsmith06@gmail.com>
2020-07-09 12:41:43 -0700

    first commit of the day. uncommented belongs_to artiust and museum in the painting model

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,6 +1,6 @@
 class Painting < ApplicationRecord
     has_one_attached :image
     belogns_to :user
-    #belongs_to :artist
-    #belongs_to :museum
+    belongs_to :artist
+    belongs_to :museum
 end
