a18271ccd4da7b454e0833e7948c6587252675de
logansmith6 <logsmith06@gmail.com>
2020-07-09 13:44:51 -0700

    redid migrations. adding artist_name= method to painting.rb

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ApplicationRecord
     has_many :paintings
     has_many :museums, through: :paintings
+
+    
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -3,6 +3,12 @@ class Painting < ApplicationRecord
     belongs_to :user
     belongs_to :artist
     belongs_to :museum
+    belongs_to :artwork_era
+
 
     accepts_nested_attributes_for :artist
+
+    def artist_name=(name)
+        self.artist = Artist.find_or_create_by(name: name)
+    end
 end
